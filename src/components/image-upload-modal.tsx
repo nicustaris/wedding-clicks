ad3e1c0121e0dfbacef63264866284f69d500424
Nicolae Staris <nicolae.staris@gmail.com>
2025-08-11 23:41:31 +0100

    .

--- src/components/image-upload-modal.tsx
+++ src/components/image-upload-modal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { cn } from "@/lib/utils";
 
@@ -10,20 +12,39 @@ import {
   DialogClose,
 } from "@/components/ui/dialog";
 import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
 
 interface Props {
   className?: string;
 }
 
 const ImageUploadModal: React.FC<Props> = ({ className }) => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="outline">Upload photo</Button>
       </DialogTrigger>
-      <DialogContent>
-        <DialogTitle>title</DialogTitle>
-        <DialogDescription>desc</DialogDescription>
+      <DialogContent
+        className={cn(
+          "w-full max-w-2xl bg-foreground text-background",
+          className
+        )}
+      >
+        <DialogTitle>Media upload</DialogTitle>
+        <DialogDescription className="">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col space-y-2 mt-2"
+          >
+            <Input placeholder="Your name" />
+            <Textarea placeholder="Description" />
+            <Input type="file" accept="image/*,video/*" />
+          </form>
+        </DialogDescription>
       </DialogContent>
     </Dialog>
   );
